26214f571560711b8fcddee4cea420ff3db3911d
Ashfaaq90 <ashfaaq90@gmail.com>
2024-08-10 00:59:39 +0530

    Update index.php

--- v1/index.php
+++ v1/index.php
@@ -1,1 +1,39 @@
+<?php
 
+/* 
+ * Developed by Ashfaaq Mahroof
+ * GMA Printers
+ * Main System Routing File
+ */
+
+//starting page session
+session_start();
+
+//loading page name and action
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $page = preg_replace("/[^A-Za-z0-9]+/", "", $_GET['page']);
+}else{
+    $page = "home"; //default
+}
+if(isset($_GET['action']) && !empty($_GET['action'])){
+    $action = preg_replace("/[^A-Za-z0-9]+/", "", $_GET['action']);
+}else{
+    $action = "index"; //default
+}
+//debug only ***
+//echo "Page is: ".$page."<br>Action is: ".$action;
+
+//loading core files
+include 'core/core_functions.php';
+include 'core/configuration.php';
+
+//loading the requested controller
+$con_name = 'controllers/'.$page.'_controller.php';
+if(file_exists($con_name)){
+    include $con_name;
+}else{
+    include 'controllers/404_controller.php';
+}
+
+
+?>
